7b0e2b38e7ee82fafbbe660aaeb3073cc7fcf04d
Rudy Grays <Egormatveev228@inbox.ru>
2024-12-07 11:13:16 +0500

    fix: try to fix auth adapters

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,8 @@ import { AppBreadcrumb } from "@/widgets/AppBreadcrumb/ui/AppBreadcrumb";
 import { Header } from "@/widgets/Header/ui/Header";
 import Image from "next/image";
 import { AppLogo } from "@/shared/ui/app-logo";
+import { Suspense } from "react";
+import { Spinner } from "@/shared/ui/spinner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -39,20 +41,28 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
-        <AppProvider>
-          <AppSidebar />
-          <div className="w-full h-full">
-            <div className="flex">
-              <Header />
+        <Suspense
+          fallback={
+            <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+              <Spinner />
             </div>
+          }
+        >
+          <AppProvider>
+            <AppSidebar />
+            <div className="w-full h-full">
+              <div className="flex">
+                <Header />
+              </div>
 
-            <Container>
-              <main className="h-full px-2 py-2 flex justify-center">
-                {children}
-              </main>
-            </Container>
-          </div>
-        </AppProvider>
+              <Container>
+                <main className="h-full px-2 py-2 flex justify-center">
+                  {children}
+                </main>
+              </Container>
+            </div>
+          </AppProvider>
+        </Suspense>
       </body>
     </html>
   );
