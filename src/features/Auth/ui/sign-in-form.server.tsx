b231da5bd1e65afeffea0bf5d4f21dbf5c35d5ac
Rudy Grays <Egormatveev228@inbox.ru>
2024-12-09 22:50:24 +0500

    fix routes bug

--- src/features/Auth/ui/sign-in-form.server.tsx
+++ src/features/Auth/ui/sign-in-form.server.tsx
@@ -3,9 +3,17 @@ import { cn } from "@/shared/lib/utils";
 import { EmailSignInForm } from "./email-sign-in-form";
 import { Divider } from "./divider";
 import { ProviderButton } from "./provider-button";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { getAppSessionServer } from "../model/lib/get-server-session";
 
 export async function SignInForm({ className }: { className?: string }) {
   const providers = await getProviders();
+  const session = await getAppSessionServer();
+
+  if (session) {
+    redirect("/");
+  }
   const oauthProviders = Object.values(providers ?? {}).filter(
     (provider) => provider.type === "oauth"
   );
